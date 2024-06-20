95d3b68586a3d46461e9df1a305e292754037301
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-20 16:36:17 +0200

    metrix

---
+++ bin/Metrix.py
@@ -0,0 +1,73 @@
+import torchvision
+import torch
+from sklearn.metrics import auc
+
+class Metrics:
+    def __init__(self, confidence_scores):
+        self.confidence_scores=confidence_scores
+    
+    
+    def get_precision_recall(self):
+        results=[]
+        auc_score=0
+        
+        for i in range(0,len(self.confidence_scores)):
+            tp=self.confidence_scores[i][0]
+            fp=self.confidence_scores[i][1]
+            fn=self.confidence_scores[i][2]
+
+            recall = tp/(tp+fn)
+            precision = tp/(tp+fp) if (tp+fp)>0 else 0.001
+            auc_score +=  auc(recall, precision)
+
+            results.append({'confidence':i+1/10, 'precision':precision,'recall':recall,'auc':auc_score})
+        mean_auc_score=auc_score/len(self.confidence_scores)
+        return((mean_auc_score,results))
+    
+
+def get_confMatr(predictions: list[tuple], ground_truths: list[tuple], class_id: int):
+    class_prs = [torch.Tensor(bbox) for _, bbox in predictions]
+    class_grs = [torch.Tensor(bbox) for idx, bbox in ground_truths if idx == class_id]
+
+    true_positives = 0
+    false_positives = 0
+    false_negatives = 0
+
+    print("class_prs", class_prs, "class_grs", class_grs)
+
+    class_grs = torch.stack(class_grs, dim=0)
+    class_prs = torch.stack(class_prs, dim=0)
+    if class_grs.numel()!=0 and class_prs.numel()!=0:
+        # Convert lists of boxes to tensors
+        gr_bbxs = torchvision.ops.box_convert(
+            boxes=torch.Tensor(class_grs),
+            in_fmt="cxcywh",
+            out_fmt="xyxy",
+        )
+
+        pr_bbxs  = torchvision.ops.box_convert(
+            boxes=torch.Tensor(class_prs),
+            in_fmt="cxcywh",
+            out_fmt="xyxy",
+        )
+
+        # pr_bbxs = torch.stack(class_prs, dim=0)
+
+        # Find matching pairs (if any) based on IoU
+        iou_matrix = torchvision.ops.box_iou(gr_bbxs, pr_bbxs)  # Efficient IoU calculation
+        matched_indices = torch.where(
+            iou_matrix >= 0.5
+        )  # Assuming IoU threshold of 0.5
+
+        true_positives = matched_indices[0].unique().numel()
+
+        false_positives = len(class_prs) - true_positives
+
+        false_negatives = len(class_grs) - true_positives
+    elif not class_prs:
+        false_negatives = 1
+    elif not class_grs:
+        false_positives = 1
+
+    return true_positives, false_positives, false_negatives
+
