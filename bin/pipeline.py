a8d47d7113859cfef91955245a16b9b54cc14839
arthur875 <alemigliorini2006@gmail.com>
2024-06-21 14:05:30 +0000

    modified ArgumentParser description

--- bin/pipeline.py
+++ bin/pipeline.py
@@ -55,7 +55,9 @@ def process_class(list_of_images: list[Image], class_name: str, num_alias: int):
 
 
 def args_parsing():
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(
+        description='Process images to generate class aliases and predict bounding boxes using a visual model.'
+    )
     parser.add_argument('-i', '--indir', type=str, help='Input image path')
     parser.add_argument('-a', '--alias', type=int, help='Num of alias for each class')
     parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode')
