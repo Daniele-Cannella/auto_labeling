581e0cce15d7102abc9d8954de529e02b906c10e
AbitanteDiScampia <114224782+AbitanteDiScampia@users.noreply.github.com>
2024-06-24 09:41:36 +0200

    Update dataset.py

--- bin/dataset.py
+++ bin/dataset.py
@@ -2,50 +2,45 @@ import os
 import json
 from image import Image
 from alias import Alias
-from log import Log
 
 
 class Dataset():
-    images = []
-    aliases = []
-
-    def __init__(self, main_dir: str = None):
+    def __init__(self, main_dir: str):
         self.main_dir = main_dir
+        self.images = []
+        self.aliases = []
 
-
-    def __dict__(self):
-        Dataset.images = [str(image) for image in Dataset.images]
-        return {
-            'images': self.images
-        }
-
-    def _add_image(self, image: object):
+    def _add_image(self, image:object):
         if isinstance(image, Image):
             self.images.append(image)
-            print(f"Image {image} added to dataset")
+            print(f"Image added to dataset")
         else:
             raise ValueError("Errore: l'oggetto non è di tipo Image")
 
-    def _add_alias(self, alias: object):
-        if alias is type(Alias):
-            self.aliases.append(alias)
-        else:
-            raise ValueError("Errore: l'oggetto non è di tipo Alias")
+    def _add_alias(self, alias:object):
+        self.aliases.append(alias)
 
-    @classmethod
-    def save_data(cls):
+    def save_data(self):
         json_file = '../data/dataset.json'
 
         try:
-            with open(json_file, 'a') as f:
-                Dataset.images = [str(image) for image in Dataset.images]
-
-                for image in Dataset.images:
+            with (open(json_file, 'a') as f):
+                self.images = [str(image) for image in self.images]
+                for image in self.images:
                     data = {
-                        'images_saves' : Dataset.images,
-                        'aliases_saves' : Dataset.aliases
+                        'images_saves' : image,
                     }
                 json.dump(data, f, indent=4)
+
+                for alias in self.aliases:
+                    data = {
+                        'alias' : alias.alias,
+                        'class_id' : alias.class_id,
+                        'metrics' : alias.metrics
+                    }
+                json.dump(data, f, indent=4)
+                f.write("\n")
+
         except Exception as e:
             print(f"Error saving data: {e}")
 
