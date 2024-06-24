81af313302d8867b569153fd8a9cf7574c5e639a
AbitanteDiScampia <114224782+AbitanteDiScampia@users.noreply.github.com>
2024-06-24 09:57:38 +0200

    Update dataset.py

--- bin/dataset.py
+++ bin/dataset.py
@@ -10,15 +10,29 @@ class Dataset():
         self.aliases = []
 
     def _add_image(self, image:object):
+        """
+        _add_image.
+        :param image:
+        :return: None
+        """
         if isinstance(image, Image):
             self.images.append(image)
         else:
             raise ValueError("Errore: l'oggetto non è di tipo Image")
 
     def _add_alias(self, alias:object):
+        """
+        _add_alias.
+        :param alias:
+        :return: None
+        """
         self.aliases.append(alias)
 
     def save_data(self):
+        """
+        save_data.
+        :return: None
+        """
         json_file = '../data/dataset.json'
 
         try:
@@ -43,6 +57,10 @@ class Dataset():
             print(f"Error saving data: {e}")
 
     def load_data(self) -> list[dict]:
+        """
+        load_data.
+        :return: dizionaio di dati
+        """
         try:
             json_file = '../data/dataset.json'
             with open(json_file, 'r') as f:
@@ -53,6 +71,11 @@ class Dataset():
             return []
 
     def images_dir(self, dir:str) -> list[str]:
+        """
+        images_dir.
+        :param dir: directory delle immagini
+        :return: lista di path delle immagini
+        """
         list_images_path = []
         for root, dirs, files in os.walk(dir):
             for file in files:
